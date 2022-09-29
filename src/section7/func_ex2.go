d60aee71c8dee3dc2e346745cad498a5c7dcb178
Dawoon <tyranno2020@gmail.com>
2022-09-29 22:26:47 +0900

    study function 2

---
+++ src/section7/func_ex2.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+  "fmt"
+)
+
+
+func multiply(x, y int)(r int) {
+  r = x * y
+  return r
+}
+
+func sum(x, y int)(r int) {
+  r = x + y
+  return r
+}
+
+func main() {
+  //함수 고급
+  //함수를 변수에 할당
+  //예제1 (슬라이스에 할당)
+  f := []func(int, int) int{multiply, sum}
+
+  a := f[0](10, 10)
+  b := f[1](10, 10)
+
+  fmt.Println("ex1 : ", a, f[0](10, 10))
+  fmt.Println("ex1 : ", b, f[1](10, 10))
+
+  //예제2 (변수에 할당)
+  var f1 func(int, int) int = multiply
+  f2 := sum
+  // 위 두가지가 같은 방식이다.
+
+  fmt.Println("ex2 : ", a, f1(10, 10))
+  fmt.Println("ex2 : ", b, f2(10, 10))
+
+  //예제3 (맵에 할당)
+  m := map[string] func(int, int) int {
+    "mul_func" : multiply,
+    "sum_func" : sum,
+  }
+
+  fmt.Println("ex3 : ", m["mul_func"](10,10))
+  fmt.Println("ex3 : ", m["sum_func"](10,10))
+
+}
